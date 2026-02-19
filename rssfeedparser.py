c435f3fee3f48fd41a30a554970304848ef3ff61
niklastv <niklas.tverin@gmail.com>
2026-02-19 14:21:01 +0200

    Small fixes and translations, added NYTimes

--- rssfeedparser.py
+++ rssfeedparser.py
@@ -1,10 +1,11 @@
 import feedparser
 
-# Määritellään uutislähteet ja niiden RSS-osoitteet
+# Define news sources and the RSS-addresses
 UUTISLAHTEET = {
     "BBC News (World)": "http://feeds.bbci.co.uk/news/world/rss.xml",
     "Der Spiegel": "https://www.spiegel.de/index.rss",
     "Helsingin Sanomat": "https://www.hs.fi/rss/tuoreimmat.xml",
+    "NYTimes": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
     "Yle Uutiset": "https://yle.fi/rss/uutiset/paauutiset",
 }
 
@@ -16,21 +17,20 @@ def hae_ja_nayta_uutiset():
     for nimi, url in UUTISLAHTEET.items():
         print(f"\n--- {nimi.upper()} ---")
         
-        # Parsitaan syöte
+        # Let's parse the feed
         syote = feedparser.parse(url)
         
-        # Tarkistetaan, saatiinko uutisia (varmistetaan ettei linkki ole rikki)
+        # Let's check if we got news (and the addresses are correct)
         if not syote.entries:
             print("Uutisia ei voitu hakea juuri nyt.")
             continue
 
-        # Otetaan 5 ensimmäistä uutista
+        # Show five latest news
         for i, uutinen in enumerate(syote.entries[:5], 1):
             print(f"{i}. {uutinen.title}")
             # print(f"   Lue lisää: {uutinen.link}") # Poista kommentti jos haluat linkit
 
     print("\n" + "=" * 40)
-    print("Haku valmis.")
 
 if __name__ == "__main__":
     hae_ja_nayta_uutiset()
